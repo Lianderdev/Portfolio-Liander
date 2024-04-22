fccc0b312920b24a434b8960cceded6c97da6cba
Lianderdev <liandervinicius13@gmail.com>
2024-04-22 16:17:55 -0300

    efeitos

---
+++ script.js
@@ -0,0 +1,76 @@
+window.revel = ScrollReveal({reset: true})
+
+revel.reveal('header', {
+    duration: 1000, 
+    distance: '-90px'
+    //delay: 500 = meio segundo
+})
+
+revel.reveal('.title-sections', {
+    origin: 'left',
+    duration: 1000, 
+    distance: '90px'
+    //delay: 500 = meio segundo
+})
+
+revel.reveal('#about', {
+    duration: 2000, 
+    distance: '20px'
+    //delay: 500 = meio segundo
+})
+
+
+revel.reveal('#projects', {
+    duration: 2000, 
+    distance: '20px'
+    //delay: 500 = meio segundo
+})
+
+revel.reveal('#education', {
+    duration: 2000, 
+    distance: '20px'
+
+})
+
+revel.reveal('#contact', {
+    duration: 2000, 
+    distance: '20px'
+    //delay: 500 = meio segundo
+})
+
+revel.reveal('footer', {
+    duration: 1000, 
+    distance: '90px'
+    //delay: 500 = meio segundo
+})
+
+revel.reveal('.visibilit-1', {
+    duration: 1000, 
+    //delay: 500 = meio segundo
+})
+
+revel.reveal('.visibilit-2', {
+    duration: 1000, 
+    delay: 200
+})
+
+revel.reveal('.visibilit-3', {
+    duration: 1000, 
+    delay: 300
+})
+
+revel.reveal('.visibilit-4', {
+    duration: 1000, 
+    delay: 400
+})
+
+revel.reveal('.visibilit-5', {
+    duration: 1000, 
+    delay: 600
+})
+
+revel.reveal('.visibilit-6', {
+    duration: 1000, 
+    delay: 800
+})
+
