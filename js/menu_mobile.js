23978211007b2cf1bebfaa0e855bb52ece09bc4f
Liander Souza <liandervinicius13@gmail.com>
2024-05-15 11:16:55 -0300

    estilizando header e menu mobile

--- js/menu_mobile.js
+++ js/menu_mobile.js
@@ -1,12 +1,20 @@
 const AbrirMenuMobile = document.querySelector('.button-menu-mobile')
 const FecharMenuMobile = document.querySelector('.fechar-menu-mobile')
 const MenuNavegaçao = document.querySelector('.menu-nav')
+const overlay = document.querySelector('.overlay')
 
 
 AbrirMenuMobile.addEventListener('click', () => {
     MenuNavegaçao.classList.add('abrir-menu')
+    overlay.style.display = 'block'
 })
 
 FecharMenuMobile.addEventListener('click', () => {
     MenuNavegaçao.classList.remove('abrir-menu')
+    overlay.style.display = 'none'
+})
+
+overlay.addEventListener('click', () => {
+    MenuNavegaçao.classList.remove('abrir-menu')
+    overlay.style.display = 'none'
 })
